944e58860e28d64fc6ba6c3fc56cda7a6537ceec
Donato-Carola <don94@live.it>
2024-01-12 17:50:45 +0100

    paragrafo censurato con la parola inserita dall'utente

--- index.php
+++ index.php
@@ -1,17 +1,45 @@
+<!-- primo Esercizio con pragrafo  non censurato -->
+
 <?php 
  
-$word = "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Totam ex repellendus aperiam, fugit temporibus deleniti, incidunt reiciendis voluptates voluptatum nam in optio commodi obcaecati omnis eligendi, sunt voluptatem officia autem!";
-
-
+$parag = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo mollitia vel, itaque enim nostrum a dicta cum tempore sint? Possimus porro cum quas? Ratione, tempora blanditiis! Aut excepturi doloribus iure.";
 
 ?>
 
 <h1>
-    
-   <?php echo $word ?>
-   <p>lunghezza <?php echo strlen($word)  ?></p>
+
+   <?php echo $parag ?>
+   <p>lunghezza <?php echo strlen($parag)  ?></p>
 </h1>
 
 
+<!-- Esercizio con pragrafo censurato -->
+
+
+<form action="./index.php" method="GET">
+    <div>
+        <label for="words">inserisci parola da censurare </label>
+        <input type="text" name="words" id="words">
+    </div>
+</form>
+
+
+<?php
+    
+    $words = $_GET['words'];
+    
+    $paragrafocensurato = str_ireplace($words, "***", $parag )
+?>
+
+
+
+<h1>
+   paragrafo con censurare
+   <?php
+    echo $paragrafocensurato
+   ?>
+  <br>
+   <h1>lunghezza paragrafo<?php echo strlen($paragrafocensurato) ?></h1>
+</h1>
 
 
